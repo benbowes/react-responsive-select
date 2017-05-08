87f795848aff42a510fd95fb337993714394dc04
Ben Bowes <bb@benbowes.com>
2017-05-09 03:01:13 +1000

    Make options panel scroll to very top when first item auto-selected

--- src/lib/scrollIntoViewIIHOC.js
+++ src/lib/scrollIntoViewIIHOC.js
@@ -17,6 +17,7 @@ const scrollIntoViewIIHOC = WrappedComponent => class extends WrappedComponent {
   scrollIntoView() {
     this.scrollPaneDOM = this.scrollPaneDOM || this.props.scrollIntoViewScrollPaneRef();
     this.elementDOM = this.elementDOM || ReactDOM.findDOMNode(this);
+
     if (
       !this.props.isDragging &&
       this.elementDOM.classList.contains(this.props.scrollIntoViewElementSelector) &&
@@ -35,6 +36,12 @@ const scrollIntoViewIIHOC = WrappedComponent => class extends WrappedComponent {
       if (topOfElement < topOfScrollPane ) {
         this.scrollPaneDOM.scrollTop -= this.elementDOM.offsetHeight;
       }
+
+      // Scroll to show first option if first option selected
+      if (this.elementDOM.getAttribute('data-key') === '0') {
+        this.scrollPaneDOM.scrollTop = 0;
+      }
+
       // Else - no need to scroll
     }
   }
