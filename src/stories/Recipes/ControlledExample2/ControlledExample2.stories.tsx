b4050085437e6afe05ab760f9f002040d46042ec
Ben Bowes <bb@benbowes.com>
2019-05-19 21:44:28 +1000

    feat: Remove unneeded React.Context from controlled example

--- src/stories/Recipes/ControlledExample2/ControlledExample2.stories.tsx
+++ src/stories/Recipes/ControlledExample2/ControlledExample2.stories.tsx
@@ -2,9 +2,8 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { IOption } from '../../../ReactResponsiveSelect';
 import { withStoryBookInfo } from '../../../utils/withStoryBookInfo';
-import { Form } from "./Form";
-import { Context } from "./Context";
-import { BRANDS, MODELS, COLOURS } from "./mockProps";
+import { Form } from './Form';
+import { BRANDS, COLOURS, MODELS, TColoursOption, TModelsOption } from './mockProps';
 
 import './Controlled.css';
 
@@ -20,16 +19,16 @@ interface IState {
         handleChangeModel: (newValue: IOption) => void;
         handleChangeColour: (newValue: IOption) => void;
         handleSubmit: (event: any) => void;
-    }
+    };
 }
 
 class ControlledExampleApp extends React.Component<{}, IState> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            selectedBrand: "null",
-            selectedModel: "null",
-            selectedColour: "null",
+            selectedBrand: 'null',
+            selectedModel: 'null',
+            selectedColour: 'null',
             brands: BRANDS,
             models: MODELS,
             colours: COLOURS,
@@ -37,74 +36,58 @@ class ControlledExampleApp extends React.Component<{}, IState> {
                 handleChangeBrand: this.handleChangeBrand,
                 handleChangeModel: this.handleChangeModel,
                 handleChangeColour: this.handleChangeColour,
-                handleSubmit: this.handleSubmit
-            }
+                handleSubmit: this.handleSubmit,
+            },
         };
     }
 
     public handleChangeBrand = (newValue: IOption): void => {
-        const models = MODELS.filter(
-            option => option.brand === newValue.value || option.value === "null"
+        const models = MODELS.filter((option: TModelsOption & { brand: string }) =>
+            option.brand === newValue.value || option.value === 'null',
         );
 
-        const colours = COLOURS.filter(
-            option =>
-                option.brands.some(brand => brand === newValue.value) ||
-                option.value === "null"
+        const colours = COLOURS.filter((option: TColoursOption) =>
+                option.brands.some((brand: string) => brand === newValue.value) ||
+                option.value === 'null',
         );
 
-        this.setState(
-            {
-                selectedBrand: newValue.value,
-                selectedModel: "null",
-                selectedColour: "null",
-                models,
-                colours
-            },
-            () => console.log("handleChangeBrand()", this.state)
-        );
-    };
+        this.setState({
+            selectedBrand: newValue.value,
+            selectedModel: 'null',
+            selectedColour: 'null',
+            models,
+            colours,
+        });
+    }
 
     public handleChangeModel = (newValue: IOption): void => {
-        const selectedBrand = MODELS.find(model => model.value === newValue.value)
-            .brand;
+        const selectedBrand = MODELS.find((model: TModelsOption) =>
+            model.value === newValue.value,
+        ).brand;
 
-        const colours = COLOURS.filter(
-            option =>
-                option.brands.some(brand => brand === selectedBrand) ||
-                option.value === "null"
+        const colours = COLOURS.filter((option: TColoursOption) =>
+            option.brands.some((brand: string) => brand === selectedBrand) ||
+            option.value === 'null',
         );
 
-        this.setState(
-            {
-                selectedModel: newValue.value,
-                colours
-            },
-            () => console.log("handleChangeModel()", this.state)
-        );
-    };
+        this.setState({
+            selectedModel: newValue.value,
+            colours,
+        });
+    }
 
     public handleChangeColour = (newValue: IOption): void => {
-        this.setState(
-            {
-                selectedColour: newValue.value
-            },
-            () => console.log("handleChangeColour()", this.state)
-        );
-    };
+        this.setState({
+            selectedColour: newValue.value,
+        });
+    }
 
     public handleSubmit = (event: any): void => {
-        console.log("handleSubmit()", this.state);
-    };
+        console.log('handleSubmit()', this.state, event);
+    }
 
-    public render(): React.ReactNode {
-        return (
-            <>
-                <Context.Provider value={this.state}>
-                    <Form />
-                </Context.Provider>
-            </>
-        );
+    public render(): React.ReactElement {
+        return <Form {...this.state} />;
     }
 }
 
