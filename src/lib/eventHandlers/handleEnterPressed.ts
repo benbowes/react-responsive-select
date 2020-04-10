6e324a863f62ff44b05fd83bc218d27ba1418b70
Ben Bowes <bb@benbowes.com>
2020-04-10 15:20:25 +1000

    fix: guard agaignst empty value

--- src/lib/eventHandlers/handleEnterPressed.ts
+++ src/lib/eventHandlers/handleEnterPressed.ts
@@ -1,6 +1,6 @@
-import * as actionTypes from '../../constants/actionTypes';
-import ReactResponsiveSelect from '../../ReactResponsiveSelect';
-import { IProps, IState } from '../../types/';
+import * as actionTypes from "../../constants/actionTypes";
+import ReactResponsiveSelect from "../../ReactResponsiveSelect";
+import { IProps, IState } from "../../types/";
 
 interface IArgs {
   event: KeyboardEvent;
@@ -9,7 +9,12 @@ interface IArgs {
   RRSClassRef: ReactResponsiveSelect;
 }
 
-export function handleEnterPressed({ event, state, props, RRSClassRef }: IArgs): void {
+export function handleEnterPressed({
+  event,
+  state,
+  props,
+  RRSClassRef,
+}: IArgs): void {
   const {
     disabled,
     isOptionsPanelOpen,
@@ -20,15 +25,23 @@ export function handleEnterPressed({ event, state, props, RRSClassRef }: IArgs):
 
   if (disabled) return;
 
-  const value = parseFloat((event.target as any).getAttribute('data-key'));
+  const value = parseFloat((event.target as any).getAttribute("data-key"));
 
-  if (options[value] && (options[value].disabled === true || options[value].optHeader === true)) {
+  console.log("options", JSON.stringify(options));
+
+  if (
+    options[value] &&
+    (options[value].disabled === true || options[value].optHeader === true)
+  ) {
     return;
   }
 
   if (multiselect) {
     const isExistingSelection = state.multiSelectSelectedOptions.options.some(
-      (option) => option.value === options[value].value
+      (option) =>
+        options[value] &&
+        options[value].hasOwnProperty("value") &&
+        option.value === options[value].value
     );
 
     if (!isExistingSelection && props.onSelect) {
@@ -36,14 +49,14 @@ export function handleEnterPressed({ event, state, props, RRSClassRef }: IArgs):
     } else if (isExistingSelection && props.onDeselect) {
       props.onDeselect(options[value]);
     }
-    
+
     RRSClassRef.updateState({
       type: actionTypes.SET_MULTISELECT_OPTIONS,
       value: nextPotentialSelectionIndex,
     });
   } else {
-    if (props.onSelect){
-      props.onSelect(options[value])
+    if (props.onSelect) {
+      props.onSelect(options[value]);
     }
 
     RRSClassRef.updateState({
